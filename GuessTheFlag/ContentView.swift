e93bcc4c24e6fcff7b136a471b35ccc3836950ee
Eihab <mastereihab@gmail.com>
2019-10-25 18:23:32 +0100

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Eihab Khan on 25/10/2019.
+//  Copyright © 2019 Eihab Khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
